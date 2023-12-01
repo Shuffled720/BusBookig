05643445b23ff4b5067a6f8ba583efb13f305afc
PrahladPawar17 <prahlad.pawar@mittarv.com>
2023-12-01 19:08:19 +0530

    clean code

--- backened/src/index.js
+++ backened/src/index.js
@@ -12,12 +12,11 @@ app.use(cors())
 
 
 const { register, login } = require("./controllers/auth.controller");
-const postsController = require("./controllers/post.controller");
 const passport = require("./configs/passport.config");
 
 app.post("/register", register);
 app.post("/login", login);
-app.use("/posts", postsController);
+
 
 app.use(passport.initialize());
 
@@ -60,17 +59,15 @@ app.get(
 
 
 
-const carController = require("./controllers/car.controller");
+
 const busController = require("./controllers/bus.controller");
-const tempoController = require("./controllers/tempo.controller");
 const orderController = require("./controllers/order.controller");
 const bookingController = require('./controllers/booking.controller');
 const passengerController = require('./controllers/passenger.controller');
 
 
-app.use("/car", carController);
+
 app.use("/bus", busController);
-app.use("/tempo", tempoController);
 app.use("/order", orderController);
 app.use('/booking', bookingController);
 app.use('/passenger', passengerController);
