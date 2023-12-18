f541df5d811a94ae8327b90cc451dfe673a15710
Shuffled720 <shuffled720@gmail.com>
2023-12-18 12:10:45 +0530

    add basic structure of pdf viewer

---
+++ my-app/src/components/ChatWrapper.tsx
@@ -0,0 +1,4 @@
+const ChatWrapper = () => {
+  return <>ChatWrapper</>;
+};
+export default ChatWrapper;
---
+++ my-app/src/components/PdfRenderer.tsx
@@ -0,0 +1,4 @@
+const PdfRenderer = () => {
+  return <>PDF Renderer</>;
+};
+export default PdfRenderer;
